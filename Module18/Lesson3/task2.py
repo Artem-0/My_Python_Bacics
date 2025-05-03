ca871754c1f2392f260bbe1b43ee73022aa0daab
Artem_Ruchiy <artiem.ruchii,99@gmail.com>
2025-05-03 23:08:42 +0300

    finished Module18

--- Module18/Lesson3/task2.py
+++ Module18/Lesson3/task2.py
@@ -1,3 +1,16 @@
 #Задача "Подсчёт слов"
 
 words_list = []
+
+for i_num in range(3):
+    print('Введите', i_num + 1, 'слово:', end = ' ')
+    # word = input().lower()
+    word = input().upper()
+    words_list.append(word)
+
+# text = input('Введите текст: ').lower().split()
+text = input('Введите текст: ').upper().split()
+
+print('\nПодсчёт слов в тексте ')
+for index in range(3):
+    print(words_list[index], ':', text.count(words_list[index]))
